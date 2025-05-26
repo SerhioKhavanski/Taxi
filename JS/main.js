f4dd5fb692b1c8aa3d1bb584c8b7620cf1b78dde
SerhioKhavanski <serhiobladestone@gmail.com>
2025-05-26 15:10:41 +0300

    Redy_animation_header

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,10 @@
-window.onload = function() {
-    document.querySelector('.glass-panel').classList.add('show');
-  };
+window.addEventListener('DOMContentLoaded', () => {
+  const glassPanel = document.querySelector('.glass-panel');
+  const heroContent = document.querySelector('.hero-content');
+
+  // Подожди немного перед анимацией, если нужно
+  setTimeout(() => {
+    glassPanel.classList.add('show');
+    heroContent.classList.add('show-content');
+  }, 200); // задержка в мс
+});
